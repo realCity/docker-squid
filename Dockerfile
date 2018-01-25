68fc624d273c634703613150ec9e4382a78153e6
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2018-01-25 16:06:45 -0200

    RC1

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,20 @@
-FROM sameersbn/ubuntu:14.04.20170123
-MAINTAINER sameer@damagehead.com
+FROM alpine
+MAINTAINER fabio.montefuscolo@gmail.com
 
-ENV SQUID_VERSION=3.3.8 \
-    SQUID_CACHE_DIR=/var/spool/squid3 \
-    SQUID_LOG_DIR=/var/log/squid3 \
-    SQUID_USER=proxy
 
-RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 80F70E11F0F0D5F10CB20E62F5DA5F09C3173AA6 \
- && echo "deb http://ppa.launchpad.net/brightbox/squid-ssl/ubuntu trusty main" >> /etc/apt/sources.list \
- && apt-get update \
- && DEBIAN_FRONTEND=noninteractive apt-get install -y squid3-ssl=${SQUID_VERSION}* \
- && mv /etc/squid3/squid.conf /etc/squid3/squid.conf.dist \
- && rm -rf /var/lib/apt/lists/*
+RUN apk update                                                \
+    && apk add squid                                          \
+    && sed -Eie '/^#/d;/^ *$/d' /etc/squid/squid.conf         \
+    && sed -i                                                 \
+        '1iaccess_log stdio:/var/log/squid/access.log squid'  \
+        /etc/squid/squid.conf                                 \
+    && rm -rf /var/cache/apk/*
 
-COPY squid.conf /etc/squid3/squid.conf
 COPY entrypoint.sh /sbin/entrypoint.sh
 RUN chmod 755 /sbin/entrypoint.sh
 
 EXPOSE 3128/tcp
-VOLUME ["${SQUID_CACHE_DIR}"]
+VOLUME ["/var/cache/squid"]
 ENTRYPOINT ["/sbin/entrypoint.sh"]
+CMD ["/usr/sbin/squid", "-f", "/etc/squid/squid.conf", "-NYCd", "1"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,45 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-create_log_dir() {
-  mkdir -p ${SQUID_LOG_DIR}
-  chmod -R 755 ${SQUID_LOG_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG_DIR}
-}
-
-create_cache_dir() {
-  mkdir -p ${SQUID_CACHE_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
-}
-
-apply_backward_compatibility_fixes() {
-  if [[ -f /etc/squid3/squid.user.conf ]]; then
-    rm -rf /etc/squid3/squid.conf
-    ln -sf /etc/squid3/squid.user.conf /etc/squid3/squid.conf
-  fi
-}
-
-create_log_dir
-create_cache_dir
-apply_backward_compatibility_fixes
-
-# allow arguments to be passed to squid3
-if [[ ${1:0:1} = '-' ]]; then
-  EXTRA_ARGS="$@"
-  set --
-elif [[ ${1} == squid3 || ${1} == $(which squid3) ]]; then
-  EXTRA_ARGS="${@:2}"
-  set --
+if [[ ! -d /var/log/squid/00 ]]; then
+  echo "Initializing cache..."
+  /usr/sbin/squid -N -f /etc/squid/squid.conf -z
 fi
 
-# default behaviour is to launch squid
-if [[ -z ${1} ]]; then
-  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
-    echo "Initializing cache..."
-    $(which squid3) -N -f /etc/squid3/squid.conf -z
-  fi
-  echo "Starting squid3..."
-  exec $(which squid3) -f /etc/squid3/squid.conf -NYCd 1 ${EXTRA_ARGS}
+if [ "$(basename $1)" == "squid" ]; then
+    trap 'rm -f /var/log/squid/access.log' EXIT SIGINT SIGTERM
+    su -s /bin/sh squid -c 'mkfifo /var/log/squid/access.log'
+    cat /var/log/squid/access.log &
+    $@
 else
-  exec "$@"
+    exec "$@"
 fi
